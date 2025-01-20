f62b93bce0ee433946ac3d3874ce751fba210d3b
Olman Urena Herrera <olmaneuh@Olmans-MacBook-Pro.local>
2025-01-20 17:57:53 -0600

    Adding caching to Canada Dashboard Demo

--- demos/canada-population-dashboard.py
+++ demos/canada-population-dashboard.py
@@ -8,82 +8,35 @@ import matplotlib.pyplot as plt
 RAW_URL = "https://raw.githubusercontent.com/marcopeix/MachineLearningModelDeploymentwithStreamlit/master/12_dashboard_capstone/data/quarterly_canada_population.csv"
 SOURCE_URL = "https://www150.statcan.gc.ca/t1/tbl1/en/tv.action?pid=1710000901"
 
-df = pd.read_csv(
-    RAW_URL,
-    dtype={
-        "Quarter": str,
-        "Canada": np.int32,
-        "Newfoundland and Labrador": np.int32,
-        "Prince Edward Island": np.int32,
-        "Nova Scotia": np.int32,
-        "New Brunswick": np.int32,
-        "Quebec": np.int32,
-        "Ontario": np.int32,
-        "Manitoba": np.int32,
-        "Saskatchewan": np.int32,
-        "Alberta": np.int32,
-        "British Columbia": np.int32,
-        "Yukon": np.int32,
-        "Northwest Territories": np.int32,
-        "Nunavut": np.int32,
-    },
-)
 
+@st.cache_data
+def load_data():
+    df = pd.read_csv(
+        RAW_URL,
+        dtype={
+            "Quarter": str,
+            "Canada": np.int32,
+            "Newfoundland and Labrador": np.int32,
+            "Prince Edward Island": np.int32,
+            "Nova Scotia": np.int32,
+            "New Brunswick": np.int32,
+            "Quebec": np.int32,
+            "Ontario": np.int32,
+            "Manitoba": np.int32,
+            "Saskatchewan": np.int32,
+            "Alberta": np.int32,
+            "British Columbia": np.int32,
+            "Yukon": np.int32,
+            "Northwest Territories": np.int32,
+            "Nunavut": np.int32,
+        },
+    )
 
-# title
-st.title("Population of Canada")
-st.write(f"Source table can be found [here]({SOURCE_URL}).")
-
-
-# display data
-with st.expander("See full data table"):
-    st.write(df)
-
-
-# display form
-with st.form("population-form"):
-
-    col1, col2, col3 = st.columns(3)
-
-    with col1:
-        st.write("Choose a starting date")
-        start_quarter = st.selectbox(
-            label="Quarter", options=["Q1", "Q2", "Q3", "Q4"], index=2, key="start_q"
-        )
-        start_year = st.slider(
-            label="Year",
-            min_value=1991,
-            max_value=2023,
-            value=1991,
-            step=1,
-            key="start_y",
-        )
-    with col2:
-        st.write("Choose an end date")
-        end_quarter = st.selectbox(
-            label="Quarter", options=["Q1", "Q2", "Q3", "Q4"], index=0, key="end_q"
-        )
-        end_year = st.slider(
-            label="Year",
-            min_value=1991,
-            max_value=2023,
-            value=2023,
-            step=1,
-            key="end_y",
-        )
-    with col3:
-        st.write("Choose a location")
-        location = st.selectbox(
-            label="Choose a location", options=df.columns[1:], index=0
-        )
-
-    submit_btn = st.form_submit_button("Analyze")
-
-start_date = f"{start_quarter} {start_year}"
-end_date = f"{end_quarter} {end_year}"
+    return df
 
 
 # utility methods
+@st.cache_data
 def format_date_for_comparison(date):
     if date[1] == 2:
         return float(date[2:]) + 0.25
@@ -95,6 +48,7 @@ def format_date_for_comparison(date):
         return float(date[2:])
 
 
+@st.cache_data
 def end_before_start(start_date, end_date):
     num_start_date = format_date_for_comparison(start_date)
     num_end_date = format_date_for_comparison(end_date)
@@ -155,10 +109,70 @@ def display_dashboard(start_date, end_date, location):
         st.pyplot(fig)
 
 
-# validations and dashboard display
-if start_date not in df["Quarter"].tolist() or end_date not in df["Quarter"].tolist():
-    st.error("No data available. Check your quarter and year selection")
-elif end_before_start(start_date, end_date):
-    st.error("Dates don't work. Start date must come before end date.")
-else:
-    display_dashboard(start_date, end_date, location)
+if __name__ == "__main__":
+    # load the data
+    df = load_data()
+
+    # title
+    st.title("Population of Canada")
+    st.write(f"Source table can be found [here]({SOURCE_URL}).")
+
+    # display data
+    with st.expander("See full data table"):
+        st.write(df)
+
+    # display form
+    with st.form("population-form"):
+
+        col1, col2, col3 = st.columns(3)
+
+        with col1:
+            st.write("Choose a starting date")
+            start_quarter = st.selectbox(
+                label="Quarter",
+                options=["Q1", "Q2", "Q3", "Q4"],
+                index=2,
+                key="start_q",
+            )
+            start_year = st.slider(
+                label="Year",
+                min_value=1991,
+                max_value=2023,
+                value=1991,
+                step=1,
+                key="start_y",
+            )
+        with col2:
+            st.write("Choose an end date")
+            end_quarter = st.selectbox(
+                label="Quarter", options=["Q1", "Q2", "Q3", "Q4"], index=0, key="end_q"
+            )
+            end_year = st.slider(
+                label="Year",
+                min_value=1991,
+                max_value=2023,
+                value=2023,
+                step=1,
+                key="end_y",
+            )
+        with col3:
+            st.write("Choose a location")
+            location = st.selectbox(
+                label="Choose a location", options=df.columns[1:], index=0
+            )
+
+        submit_btn = st.form_submit_button("Analyze")
+
+    start_date = f"{start_quarter} {start_year}"
+    end_date = f"{end_quarter} {end_year}"
+
+    # validations and dashboard display
+    if (
+        start_date not in df["Quarter"].tolist()
+        or end_date not in df["Quarter"].tolist()
+    ):
+        st.error("No data available. Check your quarter and year selection")
+    elif end_before_start(start_date, end_date):
+        st.error("Dates don't work. Start date must come before end date.")
+    else:
+        display_dashboard(start_date, end_date, location)
